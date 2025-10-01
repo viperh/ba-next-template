442fe5bc392004b55e26e7d9b6f0fdbc0413e398
qviperh <qviperh.dev@proton.me>
2025-10-01 23:22:08 +0200

    init

---
+++ src/lib/auth/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react"
+export const {signIn, signOut, useSession} = createAuthClient({
+	/** The base URL of the server (optional if you're using the same domain) */
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+})
+
+
+
